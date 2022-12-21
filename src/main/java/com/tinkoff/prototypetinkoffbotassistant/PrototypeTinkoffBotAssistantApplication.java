efa52654709e6c4457654df668aef872b5c5e854
Sergo Kovaltsov <sergeykovaltsov@gmail.com>
2022-12-21 11:19:35 -0500

    Initial Commit - Setting up Gradle dependencies

---
+++ src/main/java/com/tinkoff/prototypetinkoffbotassistant/PrototypeTinkoffBotAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.tinkoff.prototypetinkoffbotassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypeTinkoffBotAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrototypeTinkoffBotAssistantApplication.class, args);
+    }
+
+}
